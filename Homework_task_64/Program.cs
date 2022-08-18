972694d1e8f15229278cd2a149011f81db003b58
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-08-18 15:46:59 +0300

    Homework Seminar 9

---
+++ Homework_task_64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: 
+// Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 6, 7, 8"
+
+int m = 4;
+int n = 8;
+
+string PrintNumbers(int start, int end)
+{
+    if (start==end)
+        return start.ToString();
+    return (start + " " + PrintNumbers(start+1, end));
+}
+
+Console.Write(PrintNumbers(m,n));
---
+++ Homework_task_66_Sum/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: 
+// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int m = 1;
+int n = 15;
+
+int FindSum(int start, int end)
+{
+    if (start==end)
+        return start;
+    return (start + FindSum(start+1, end));
+}
+
+Console.Write(FindSum(m,n));
